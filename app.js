61f8908b6c3835a495d80ba9f62b322e7683b945
Akshay Gadegone <akshay.gadegone@gmail.com>
2019-11-22 18:45:25 -0800

    Initial

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+
+app.get('/akiMedium', function(request, response) {
+    var request = require('request');
+      var https = require('https');
+      var parseString = require('xml2js').parseString;
+      var xml = '';
+    function xmlToJson(url, callback) {
+      var req = https.get(url, function(res) {
+        var xml = '';
+    res.on('data', function(chunk) {
+          xml += chunk;
+        });
+    res.on('error', function(e) {
+          callback(e, null);
+        });
+    res.on('timeout', function(e) {
+          callback(e, null);
+        });
+    res.on('end', function() {
+          parseString(xml, function(err, result) {
+            callback(null, result);
+          });
+        });
+      });
+    }
+    var url = "https://medium.com/feed/@TheBBP_eds"
+    xmlToJson(url, function(err, data) {
+      if (err) {
+        return console.err(err);
+      }
+     var jsonString = JSON.stringify(data, null, 2);
+     var objectValue = JSON.parse(jsonString);
+    
+     var g = objectValue['rss']['channel'][0];
+    
+     var resultArray = [];
+
+     for(var item in g['item']){
+        var yourString = g['item'][item]['content:encoded'].toString();
+        var maxLength = 200;
+        var trimmedString = yourString.substr(0, maxLength);
+        var regEx = /\b(https?:\/\/.*?\.[a-z]{2,4}\/[^\s]*\b)/g;
+        var data = {
+          authImg: trimmedString.match(regEx),
+          title: g['item'][item]['title'],
+          link: g['item'][item]['link'],
+          desc: g['item'][item]['content:encoded']
+        };
+        resultArray.push(data);
+      }
+      response.send(resultArray);
+     });
+    });
+
+app.listen(2700);
