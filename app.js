b1fbe377998cb171c702f24181780ad7681c51ea
akshayscu <akshay.gadegone@gmail.com>
2019-11-27 14:08:27 -0800

    added route to the app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(cors());
 
-app.get('/akiMedium', function(request, response) {
+app.get('/bbpMedium', function(request, response) {
     var request = require('request');
       var https = require('https');
       var parseString = require('xml2js').parseString;
@@ -28,7 +28,7 @@ app.get('/akiMedium', function(request, response) {
         });
       });
     }
-    var url = "https://medium.com/feed/@aki9154"
+    var url = "https://medium.com/feed/the-better-because-project"
     xmlToJson(url, function(err, data) {
       if (err) {
         return console.err(err);
@@ -64,8 +64,56 @@ app.get('/akiMedium', function(request, response) {
      });
     });
 
+    app.get('/akiMedium', function(request, response) {
+      var request = require('request');
+        var https = require('https');
+        var parseString = require('xml2js').parseString;
+        var xml = '';
+      function xmlToJson(url, callback) {
+        var req = https.get(url, function(res) {
+          var xml = '';
+      res.on('data', function(chunk) {
+            xml += chunk;
+          });
+      res.on('error', function(e) {
+            callback(e, null);
+          });
+      res.on('timeout', function(e) {
+            callback(e, null);
+          });
+      res.on('end', function() {
+            parseString(xml, function(err, result) {
+              callback(null, result);
+            });
+          });
+        });
+      }
+      var url = "https://medium.com/feed/@aki9154"
+      xmlToJson(url, function(err, data) {
+        if (err) {
+          return console.err(err);
+        }
+       var jsonString = JSON.stringify(data, null, 2);
+       var objectValue = JSON.parse(jsonString);
+       var g = objectValue['rss']['channel'][0];
+       var resultArray = [];
+       for(var item in g['item']){
+          var data = {
+            title: g['item'][item]['title'],
+            link: g['item'][item]['link'],
+            description : g['item'][item]['content:encoded']
+          };
+          resultArray.push(data);
+        }
+        response.send(resultArray);
+       });
+      });
+
+
     var port = process.env.PORT;
     if (port == null || port == "") {
       port = 2900;
     }
     app.listen(port);
+
+
