1dc12b23af065a0e3a5ec7bc6e907922ce0e7c98
akshayscu <akshay.gadegone@gmail.com>
2019-11-27 19:21:56 -0800

    added a new route...

--- app.js
+++ app.js
@@ -110,6 +110,53 @@ app.get('/bbpMedium', function(request, response) {
        });
       });
 
+      app.get('/susMedium', function(request, response) {
+        var request = require('request');
+          var https = require('https');
+          var parseString = require('xml2js').parseString;
+          var xml = '';
+        function xmlToJson(url, callback) {
+          var req = https.get(url, function(res) {
+            var xml = '';
+        res.on('data', function(chunk) {
+              xml += chunk;
+            });
+        res.on('error', function(e) {
+              callback(e, null);
+            });
+        res.on('timeout', function(e) {
+              callback(e, null);
+            });
+        res.on('end', function() {
+              parseString(xml, function(err, result) {
+                callback(null, result);
+              });
+            });
+          });
+        }
+        var url = "https://medium.com/feed/@susannapage"
+        xmlToJson(url, function(err, data) {
+          if (err) {
+            return console.err(err);
+          }
+         var jsonString = JSON.stringify(data, null, 2);
+         var objectValue = JSON.parse(jsonString);
+         var g = objectValue['rss']['channel'][0];
+         var resultArray = [];
+         for(var item in g['item']){
+            var data = {
+              img: ['https://upload.wikimedia.org/wikipedia/commons/4/42/Blog_%281%29.jpg'],
+              title: g['item'][item]['title'],
+              link: g['item'][item]['link'],
+              desc : g['item'][item]['content:encoded']
+            };
+            resultArray.push(data);
+          }
+          response.send(resultArray);
+         });
+        });
+  
+
 
     var port = process.env.PORT;
     if (port == null || port == "") {
